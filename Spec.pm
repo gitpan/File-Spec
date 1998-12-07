3f758c52ad05e1b787cba20c3fc4c5e2d2f4f716
Kenneth Albanowski <kjahds@kjahds.com>
1998-12-07 11:24:35 +0000

    Import of KJALB/File-Spec-0.7 from CPAN.
    gitpan-cpan-distribution: File-Spec
    gitpan-cpan-version:      0.7
    gitpan-cpan-path:         KJALB/File-Spec-0.7.tar.gz
    gitpan-cpan-author:       KJALB
    gitpan-cpan-maturity:     released

--- Spec.pm
+++ Spec.pm
@@ -14,7 +14,7 @@ require Exporter;
 use strict;
 use vars qw(@ISA $VERSION $Verbose);
 
-$VERSION = '0.6';
+$VERSION = '0.7';
 
 $Verbose = 0;
 
@@ -52,11 +52,15 @@ File::Spec - portably perform operations on file names
 
 =head1 SYNOPSIS
 
-C<use File::Spec;>
+	use File::Spec;
 
-C<$x=File::Spec-E<gt>catfile('a','b','c');>
+	$x=File::Spec->catfile('a', 'b', 'c');
 
-which returns 'a/b/c' under Unix.
+which returns 'a/b/c' under Unix. Or:
+
+	use File::Spec::Functions;
+	
+	$x = catfile('a', 'b', 'c');
 
 =head1 DESCRIPTION
 
@@ -78,8 +82,8 @@ OS specific routines is available in a separate module, including:
 	File::Spec::VMS
 
 The module appropriate for the current OS is automatically loaded by
-File::Spec. Since some modules (like VMS) make use of OS specific
-facilities, it may not be possible to load all modules under all operating
+File::Spec. Since some modules (like VMS) make use of facilities only under
+that OS, it may not be possible to load all modules under all operating
 systems.
 
 Since File::Spec is object oriented, subroutines should not called directly,
@@ -91,9 +95,17 @@ but rather as class methods:
 
 	File::Spec->catfile('a','b');
 
+Alternatively, File::Spec::Functions can be used instead, which exports
+functions for all of the class methods provided by File::Spec, including
+catfile, so you may say:
+
+	use File::Spec::Functions;
+	
+	catfile('a','b');
+
 For a reference of available functions, pleaes consult L<File::Spec::Unix>,
-which contains the entire set, and inherited by the modules for other
-platforms. For further information, please see L<File::Spec::Mac>,
+which contains the entire set, and which is inherited by the modules for
+other platforms. For further information, please see L<File::Spec::Mac>,
 L<File::Spec::OS2>, L<File::Spec::Win32>, or L<File::Spec::VMS>.
 
 =head1 SEE ALSO
@@ -106,7 +118,7 @@ File::Spec::VMS, ExtUtils::MakeMaker
 Kenneth Albanowski <F<kjahds@kjahds.com>>, Andy Dougherty
 <F<doughera@lafcol.lafayette.edu>>, Andreas KE<ouml>nig
 <F<A.Koenig@franz.ww.TU-Berlin.DE>>, Tim Bunce <F<Tim.Bunce@ig.co.uk>>. VMS
-support by Charles Bailey <F<bailey@genetics.upenn.edu>>.  OS/2 support by
+support by Charles Bailey <F<bailey@newman.upenn.edu>>.  OS/2 support by
 Ilya Zakharevich <F<ilya@math.ohio-state.edu>>. Mac support by Paul Schinder
 <F<schinder@pobox.com>>.
 
